f8d7969efa75cf3b367a4ba449540ee4c9266c47
Aparnaaaa <168109956+aparaspberry@users.noreply.github.com>
2024-04-28 23:43:37 +0530

    Update Books.jsx

--- client/src/components/Books.jsx
+++ client/src/components/Books.jsx
@@ -6,7 +6,7 @@ import '../css/Book.css'
 const Books = ({role}) => {
   const [books, setBooks] = useState([])
   useEffect(() => {
-    axios.get('https://localhost:3001/book/books')
+    axios.get('https://localhost:3001/book/books')  // GET method to return the Books from database.
       .then(res => {
         setBooks(res.data)
         console.log(res.data)
